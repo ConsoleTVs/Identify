21b8abb146b30a124df2737e46f217272d7039a4
Erik Campobadal <ConsoleTVs@gmail.com>
2017-04-24 19:27:25 +0200

    Update IdentityFacade.php

--- src/Facades/IdentityFacade.php
+++ src/Facades/IdentityFacade.php
@@ -11,7 +11,7 @@ class IdentityFacade extends Facade
    *
    * @return string
    */
-  protected static function getFacadeAccessor() : string
+  protected static function getFacadeAccessor()
   {
       return 'laravel-identify';
   }
